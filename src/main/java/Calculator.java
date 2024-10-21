86b2ac4ca2bf8846cc0300e1b2ca9ba66f4ed95c
HYH <jijinny26@gmail.com>
2024-10-21 09:17:03 -0500

    feat(Calculator): add user input string functionality

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+    }
+}
