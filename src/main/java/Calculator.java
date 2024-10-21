1ca3d7bf7b60747b524c63a67437941d64d66b00
HYH <jijinny26@gmail.com>
2024-10-21 09:25:00 -0500

    feat(calculator): add splitInput method for input parsing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,16 @@
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+
+    private String[] splitInput(String input) {
+
+        String delimiter = ",|:";
+        return input.split(delimiter);
+    }
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        Calculator calculator = new Calculator();
     }
 }
